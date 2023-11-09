1e4b3aac038b4f3c4b786c0aeb0264a81d12b1c5
MariosIakovidis <wildshot01@gmail.com>
2023-11-09 16:33:31 +0200

    added cookie banner

---
+++ js/main.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    var cookieBanner = document.getElementById('cookie-banner');
+    var acceptCookies = document.getElementById('accept-cookies');
+    var declineCookies = document.getElementById('decline-cookies');
+  
+    // Only show the banner if the user hasn't seen it before
+    if (!localStorage.getItem('cookieBannerDisplayed')) {
+      cookieBanner.style.display = 'block';
+    }
+  
+    // When the user accepts cookies, hide the banner and remember their choice
+    acceptCookies.addEventListener('click', function() {
+      cookieBanner.style.display = 'none';
+      localStorage.setItem('cookieBannerDisplayed', 'true');
+  
+      // Enable Google Maps
+      var mapIframe = document.createElement('iframe');
+      mapIframe.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d189.2540365355575!2d22.95026864869535!3d40.62845764083103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a839ccd9ebf701%3A0xe0a530f3d322cb52!2sALEXANDER%20INNOVATION%20ZONE!5e0!3m2!1sen!2sgr!4v1699396164300!5m2!1sen!2sgr';
+      mapIframe.width = '100%';
+      mapIframe.height = '450';
+      mapIframe.style.border = '0';
+      mapIframe.allowFullscreen = '';
+      mapIframe.loading = 'lazy';
+      mapIframe.referrerPolicy = 'no-referrer-when-downgrade';
+      document.getElementById('map-placeholder').appendChild(mapIframe);
+    });
+  
+    // When the user declines cookies, just hide the banner and remember their choice
+    declineCookies.addEventListener('click', function() {
+      cookieBanner.style.display = 'none';
+      localStorage.setItem('cookieBannerDisplayed', 'true');
+    });
+  };
